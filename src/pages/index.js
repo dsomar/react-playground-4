0b2f0c7630ec420b80d81535ff5b98140a94c08f
Denis <dsomar@gmail.com>
2018-08-02 13:34:57 -0400

    whole bunch of changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,21 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems.  Prototype and build apps with React and Swift</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="Hero">
+  <div className="HeroGroup">
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    <Link to="/page-2/">Watch the video</Link>
+    <div className="Logos">
+      <img src={require('../images/logo-sketch.png')} width="50" />
+      <img src={require('../images/logo-figma.png')} width="50" />
+      <img src={require('../images/logo-studio.png')} width="50" />
+      <img src={require('../images/logo-framer.png')} width="50" />
+      <img src={require('../images/logo-react.png')} width="50" />
+      <img src={require('../images/logo-swift.png')} width="50" />
+    </div>
   </div>
+</div>
 )
 
 export default IndexPage
